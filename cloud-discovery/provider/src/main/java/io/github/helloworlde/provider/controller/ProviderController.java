ee3f63fb4ba4876019cc6375aa806e8988e8e95c
helloworlde <hellowoodes@gmail.com>
2019-05-21 17:46:19 +0800

    Exclude all management endpoint

--- cloud-discovery/provider/src/main/java/io/github/helloworlde/provider/controller/ProviderController.java
+++ cloud-discovery/provider/src/main/java/io/github/helloworlde/provider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package io.github.helloworlde.provider.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/provider")
 @RefreshScope
+@Slf4j
 public class ProviderController {
 
     @Value("${message.prefix:empty\t}")
@@ -18,6 +20,7 @@ public class ProviderController {
     @GetMapping("/echo")
     @ResponseBody
     public String echo(@RequestParam String message) {
+        log.info("The request param is {}", message);
         return messagePrefix + message;
     }
 }
