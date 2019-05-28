a3f9adbb2c6e5e38c1df3a23b4a53e146a1f5180
helloworlde <hellowoodes@gmail.com>
2019-05-28 19:05:46 +0800

    Seata worked after replace DataSource as Druid

---
+++ cloud-seata/storage-service/src/main/java/io/github/helloworlde/storage/config/DatabaseConfig.java
@@ -0,0 +1,34 @@
+package io.github.helloworlde.storage.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author HelloWood
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Bean(destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean
+    public DataSourceProxy dataSourceProxy(DruidDataSource druidDataSource) {
+        return new DataSourceProxy(druidDataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+        return sqlSessionFactoryBean.getObject();
+    }
+}
