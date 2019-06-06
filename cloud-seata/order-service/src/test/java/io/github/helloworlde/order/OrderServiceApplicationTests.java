dc86cddbc3006e3338255d7b7f53a5ece0f19919
helloworlde <hellowoodes@gmail.com>
2019-06-06 14:28:32 +0800

    Fixed issue of package name

--- cloud-seata/order-service/src/test/java/io/github/helloworlde/order/OrderServiceApplicationTests.java
+++ cloud-seata/order-service/src/test/java/io/github/helloworlde/order/OrderServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.helloworlde.storage;
+package io.github.helloworlde.order;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PayServiceApplicationTests {
+public class OrderServiceApplicationTests {
 
     @Test
     public void contextLoads() {
--- cloud-seata/pay-service/src/test/java/io/github/helloworlde/pay/PayServiceApplicationTests.java
+++ cloud-seata/pay-service/src/test/java/io/github/helloworlde/pay/PayServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.helloworlde.consumer;
+package io.github.helloworlde.pay;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class OrderServiceApplicationTests {
+public class PayServiceApplicationTests {
 
     @Test
     public void contextLoads() {
