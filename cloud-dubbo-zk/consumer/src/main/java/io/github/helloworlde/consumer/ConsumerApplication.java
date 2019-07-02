d55e9617374ee5704cd52aa008f6a0007ad42123
helloworlde <hellowoodes@gmail.com>
2019-07-02 19:31:34 +0800

    Remove not necessary code

--- cloud-dubbo-zk/consumer/src/main/java/io/github/helloworlde/consumer/ConsumerApplication.java
+++ cloud-dubbo-zk/consumer/src/main/java/io/github/helloworlde/consumer/ConsumerApplication.java
@@ -1,6 +1,5 @@
 package io.github.helloworlde.consumer;
 
-import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableDubbo
 public class ConsumerApplication {
 
     public static void main(String[] args) {
--- cloud-dubbo-zk/provider/src/main/java/io/github/helloworlde/provider/service/impl/ProviderServiceImpl.java
+++ cloud-dubbo-zk/provider/src/main/java/io/github/helloworlde/provider/service/impl/ProviderServiceImpl.java
@@ -3,6 +3,9 @@ package io.github.helloworlde.provider.service.impl;
 import io.github.helloworlde.provider.service.ProviderService;
 import org.apache.dubbo.config.annotation.Service;
 
+/**
+ * @author HelloWood
+ */
 @Service
 public class ProviderServiceImpl implements ProviderService {
 
