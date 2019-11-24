e9cf131b21c1d015ffba3695a8933ef4cb611fd2
helloworlde <hellowoodes@gmail.com>
2019-11-24 22:32:58 +0800

    Add container related configurations.

---
+++ cloud-seata-eureka/init/init.sql
@@ -0,0 +1,54 @@
+USE mysql;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '123456';
+
+DROP DATABASE IF EXISTS seata;
+CREATE DATABASE seata;
+USE seata;
+
+CREATE TABLE account
+(
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    balance          INT,
+    last_update_time DATETIME DEFAULT now() ON UPDATE now()
+);
+
+CREATE TABLE product
+(
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    price            INT,
+    stock            INT,
+    last_update_time DATETIME DEFAULT now() ON UPDATE now()
+);
+
+CREATE TABLE orders
+(
+    id               INT AUTO_INCREMENT PRIMARY KEY,
+    user_id          INT,
+    product_id       INT,
+    pay_amount       INT,
+    status           VARCHAR(100),
+    add_time         DATETIME DEFAULT now(),
+    last_update_time DATETIME DEFAULT now() ON UPDATE now()
+);
+
+INSERT INTO account (id, balance)
+VALUES (1, 10);
+INSERT INTO product (id, price, stock)
+VALUES (1, 5, 10);
+
+CREATE TABLE undo_log
+(
+    id            BIGINT(20)   NOT NULL AUTO_INCREMENT,
+    branch_id     BIGINT(20)   NOT NULL,
+    xid           VARCHAR(100) NOT NULL,
+    context       VARCHAR(128) NOT NULL,
+    rollback_info LONGBLOB     NOT NULL,
+    log_status    INT(11)      NOT NULL,
+    log_created   DATETIME     NOT NULL,
+    log_modified  DATETIME     NOT NULL,
+    ext           VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY ux_undo_log (xid, branch_id)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8;
